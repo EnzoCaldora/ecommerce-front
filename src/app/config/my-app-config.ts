5b509d8bbb2be0755e5a753c958c48f070ba77a9
Enzo Caldora <ecaldora@gmail.com>
2024-05-31 20:10:13 +0200

    feat: :lock:
    Secure frontend with HTTPS

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -5,7 +5,7 @@ export default {
   oidc: {
     clientId: '0oahf4vvfvRXQ2Gdk5d7',
     issuer: 'https://dev-46423607.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true
 
